6d1bba50c743e1c933a7f80630ff479d0069f595
Alexandre HUET <alexandre.huet@ilek.fr>
2023-07-05 18:30:23 +0200

    initial commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,10 @@
-import "../css/main.css";
-
-// JS here
+// JS Goes here - ES6 supported
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on("init", (user) => {
+    if (!user) {
+      window.netlifyIdentity.on("login", () => {
+        document.location.href = "/admin/";
+      });
+    }
+  });
+}
